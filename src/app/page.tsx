2c2b3f7591344bf2f58c5f8d80e1f34e9f861edf
Rebecca <Rebecca@elevatedbusiness.co.uk>
2026-02-03 00:55:33 +0000

    VA Portal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "50px", fontFamily: "sans-serif" }}>
-      <h1>VA Operating System</h1>
+      <h1>VA Portal</h1>
       <p>System Initialized. Hello World. Enjoy the system</p>
     </main>
   );
