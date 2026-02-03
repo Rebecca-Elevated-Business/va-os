772df4eb43f31382fb4c4fcbfa5aae13c3f8926d
Rebecca <Rebecca@elevatedbusiness.co.uk>
2026-02-03 17:09:10 +0000

    Test push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: "50px", fontFamily: "sans-serif" }}>
       <h1>VAHQ</h1>
-      <p>System Initialized. Hello World. Enjoy the system</p>
+      <p>System Initialized. Hello World. Enjoy the system. Thank you.</p>
     </main>
   );
 }
