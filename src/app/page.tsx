54e859e69a78eed7b6bbcf48a036eff1409081cb
Rebecca <Rebecca@elevatedbusiness.co.uk>
2026-02-03 19:13:43 +0000

    Redirect app homepage to marketing site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main style={{ padding: "50px", fontFamily: "sans-serif" }}>
-      <h1>VAHQ</h1>
-      <p>System Initialized. Hello World. Enjoy the system. Thank you.</p>
-    </main>
-  );
+  redirect("https://vahq.co.uk");
 }
