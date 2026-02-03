1e107518840473105d98e6fd6a1bdeaf0472e0b3
Rebecca <Rebecca@elevatedbusiness.co.uk>
2026-02-03 19:15:26 +0000

    Make homepage redirect permanent

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://vahq.co.uk",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("https://vahq.co.uk");
+  return null;
 }
